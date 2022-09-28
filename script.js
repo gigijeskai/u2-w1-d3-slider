ecec72daa577d4576fed73cb472e0ece13050d65
Luigi Indaco <luigindaco@gmail.com>
2022-09-28 17:00:23 +0200

    consegna compito

---
+++ script.js
@@ -0,0 +1,12 @@
+let immagini = 5
+let immagineVisibile = 1
+
+function prew(){
+immagineVisibile++;
+if ( immagineVisibile > immagini){
+immagineVisibile = 1;
+}
+for (let i = immagini; i > 0;i--){
+
+}
+}
